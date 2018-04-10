68d4e5969b5c8717f7ae2aaf61cd168bcb5d5b27
unknown <Shanereeve@gmail.com>
2018-04-09 23:35:37 -0600

    Landing Page Update

--- src/components/Name.js
+++ src/components/Name.js
@@ -10,25 +10,35 @@ class Name extends React.Component {
 
                     <div className='Left'>
                         <div id="HeadShot">
-                            <img src={Head_shot} alt="HeadShot" id="head-shot"/>
+                            <img src={Head_shot} alt="HeadShot" id="head-shot" />
                         </div>
                     </div>
-                    <div className='Right'>
-                        <div id="NameTag">
-                            <div>
+                    <div className='Right flex-space-between'>
+                    <div></div>
+                        <div id="NameTag ">
+
                             <h3 id="first-name">shane</h3>
                             <h3 id="last-name">REEVE</h3>
+                            <div className="flex-row flex-space-between">
+                                <h3 id="title">Full-Stack Web Developer</h3>
                                 <div className="flex-row">
-                                    <h3 id="title">Full-Stack Web Developer</h3>
-
-                                    <div className="Icon"/>
-                                    <div className="Icon"/>
-                                    <div className="Icon"/>
-
+                                    <div className="Icon" />
+                                    <div className="Icon" />
+                                    <div className="Icon" />
                                 </div>
                             </div>
-
                         </div>
+                        <div id="InfoTag">
+                            <hr />
+                            <div>
+                            
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </div>
+
                     </div>
                 </section>
             </React.Fragment>
